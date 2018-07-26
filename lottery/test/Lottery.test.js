f110cf68c0920fb0d53d6f2579388ca1a2d80c18
abhinavchawla <abhinav.chawla.95@gmail.com>
2018-07-27 00:32:23 +0530

    Initial commit. Inbox contract working, Lottery contract working, Boiler plate react youtube app working.

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -12,13 +12,30 @@ let accounts;
 beforeEach(async ()=>{
   accounts = await web3.eth.getAccounts();
   lottery = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data:bytecode})
-    .send({from: accounts[0], gas: '1000000'});
+  .deploy({data:bytecode})
+  .send({from: accounts[0], gas: '1000000'});
 });
 
 describe('Lottery Contract',()=>{
   it('deploys a contract',()=>{
     assert.ok(lottery.options.address);
+  });
+
+  it('allows a single account to enter',async ()=>{
+    await lottery.methods.enter().send({
+      from:accounts[0],
+      value: web3.utils.toWei('0.02','ether')
+    });
+
+
+
+    const players = await lottery.methods.getPlayers().call({
+      from:accounts[0]
+    })
+
+    assert.equal(players[0],accounts[0]);
+
+    assert.equal(players.length,1)
   });
 
   it('allows multiple accounts to enter',async ()=>{
@@ -47,4 +64,42 @@ describe('Lottery Contract',()=>{
 
     assert.equal(players.length,3)
   });
+  it('requires a minimum amount of ether to enter', async()=>{
+    try{
+      await lottery.methods.enter().send({
+        from:accounts[0],
+        value:'0.0001'
+      });
+      assert(false);
+    }catch(err){
+      assert.ok(err);
+    };
+  });
+
+  it('only manager can call', async()=>{
+    try{
+      await lottery.methods.pickWinner().send({
+        from:accounts[1]
+      });
+      assert(false);
+    }catch(err){
+      assert(err);
+    }
+  })
+
+  it('sends money to the winner and resets the players array', async()=>{
+    await lottery.methods.enter().send({
+      from:accounts[0],
+      value:web3.utils.toWei('2','ether')
+    });
+    const initialBalance =await web3.eth.getBalance(accounts[0]);
+
+    await lottery.methods.pickWinner().send({from:accounts[0]});
+
+    const finalBalance =await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance-initialBalance;
+    console.log(difference);
+    assert(difference>web3.utils.toWei('1.8','ether'));
+  })
+
 });
